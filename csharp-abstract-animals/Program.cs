abc3957728ee4b6a43b595b14579b0747d00a285
CalinCretu <calincretu1412@gmail.com>
2024-04-24 15:24:07 +0200

    FIX - ciclo foreach per stampare a schermo anziché stringa per stringa

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -4,25 +4,25 @@
     {
         static void Main(string[] args)
         {
-            var cane = new Cane();
-            Console.WriteLine($"Il cane quando è stanco ha la faccia così: {cane.Dormi()}");
-            Console.WriteLine($"Il cane quando è felice te lo dice così: {cane.Verso()}");
-            Console.WriteLine($"Il cane quando ha fame mangia: {cane.Mangia()}");
+            var animali = new List<Animale>
+            {
+                new Cane(),
+                new Passerotto(),
+                new Aquila(),
+                new Delfino()
+            };
 
-            var passerotto = new Passerotto();
-            Console.WriteLine($"\nIl passerotto quando è stanco ha la faccia così: {passerotto.Dormi()}");
-            Console.WriteLine($"Il passerotto quando è felice te lo dice così: {passerotto.Verso()}");
-            Console.WriteLine($"Il passerotto quando ha fame mangia: {passerotto.Mangia()}");
-
-            var aquila = new Aquila();
-            Console.WriteLine($"\nL'aquila quando è stanco ha la faccia così: {aquila.Dormi()}");
-            Console.WriteLine($"L'aquila quando è felice te lo dice così: {aquila.Verso()}");
-            Console.WriteLine($"L'aquila quando ha fame mangia: {aquila.Mangia()}");
-
-            var delfino = new Delfino();
-            Console.WriteLine($"\nIl delfino quando è stanco ha la faccia così: {delfino.Dormi()}");
-            Console.WriteLine($"Il delfino quando è felice te lo dice così: {delfino.Verso()}");
-            Console.WriteLine($"Il delfino quando ha fame mangia: {delfino.Mangia()}");
+            foreach (var animale in animali)
+            {
+                var nomeAnimale = animale.GetType().Name;
+                var dormiAnimale = animale.Dormi();
+                var versoAnimale = animale.Verso();
+                var ciboAnimale = animale.Mangia();
+                Console.WriteLine($"L'animale {nomeAnimale} quando è stanco fa la faccia così: {dormiAnimale}\n");
+                Console.WriteLine($"L'animale {nomeAnimale} fa così: {versoAnimale}\n");
+                Console.WriteLine($"L'animale {nomeAnimale} mangia: {ciboAnimale}");
+                Console.WriteLine("-----");
+            }
         }
     }
 }
